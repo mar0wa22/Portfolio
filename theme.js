0472d4bf72ae5101258f9c22441d44511cd2bcd0
mar0wa22 <meer0abd9933@gmail.com>
2026-07-27 16:12:42 +0300

    update

--- theme.js
+++ theme.js
@@ -2,18 +2,13 @@ const themeBtn = document.getElementById("theme-toggle");
 const body = document.body;
 
 if (themeBtn) {
-
-    const savedTheme = localStorage.getItem("theme");
-
-    if (savedTheme === "light") {
+    // إذا كان المستخدم اختار Light سابقًا
+    if (localStorage.getItem("theme") === "light") {
         body.classList.add("light-mode");
         themeBtn.innerHTML = "<i class='bx bx-sun'></i>";
-    } else {
-        themeBtn.innerHTML = "<i class='bx bx-moon'></i>";
     }
 
-    themeBtn.addEventListener("click", () => {
-
+    themeBtn.onclick = () => {
         body.classList.toggle("light-mode");
 
         if (body.classList.contains("light-mode")) {
@@ -23,7 +18,5 @@ if (themeBtn) {
             localStorage.setItem("theme", "dark");
             themeBtn.innerHTML = "<i class='bx bx-moon'></i>";
         }
-
-    });
-
+    };
 }
