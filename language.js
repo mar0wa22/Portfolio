6f4cdd6d9ca4366b529734a03e960b797df7a3dd
mar0wa22 <meer0abd9933@gmail.com>
2026-07-29 21:15:15 +0300

    update

--- language.js
+++ language.js
@@ -6,7 +6,6 @@ const translations = {
         projects: "Projects",
         contact: "Contact",
         engineer: "Engineer",
-        marwa: "Marwa abdelwahab"
 
         // Hero Section
         hello: "Hi, I'm Marwa",
@@ -93,7 +92,6 @@ const translations = {
         projects: "المشاريع",
         contact: "تواصل",
         engineer: "المهندسة",
-        marwa: "مروه عبد الوهاب"
 
         // Hero Section
         hello: "مرحباً، أنا مروة",
